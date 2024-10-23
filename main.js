98843da0231d6b5f1aec792871b41629c03902d1
HeberRios <68972507+HeberRios@users.noreply.github.com>
2024-10-23 11:53:28 -0600

    Add carousel functionality

--- main.js
+++ main.js
@@ -2,8 +2,15 @@
 
 const mobileNavMenuBtn = document.querySelector("#mobile-nav-btn");
 const navMenu = document.querySelector(".nav-menu-container");
+const testimonialCards = document.querySelectorAll(".testimonial-card");
+const carouselBtns = document.querySelectorAll(".carousel-btn");
 
 // FUNCTIONS
+
+function initializeCarousel() {
+    testimonialCards[1].classList.add("selected");
+    carouselBtns[1].classList.add("selected");
+}
 
 function toggleMobileNavMenu() {
     navMenu.classList.toggle("active");
@@ -17,10 +24,30 @@ function removeActiveClass() {
 
 // EVENT LISTENERS
 
+// MOBILE NAVIGATION TOGGLE FUNCTIONALITY
+
 mobileNavMenuBtn.addEventListener("click", toggleMobileNavMenu);
+
+// CHANGE THE STYLES OF MOBILE NAVIGATION AT 768PX
 
 window.addEventListener("resize", function () {
     if (window.innerWidth >= 768) {
         removeActiveClass();
     }
 });
+
+// CAROUSEL FUNCTIONALITY
+
+carouselBtns.forEach(function (button, i) {
+    button.addEventListener("click", function () {
+        testimonialCards.forEach((item) => item.classList.remove("selected"));
+        carouselBtns.forEach((btn) => btn.classList.remove("selected"));
+
+        testimonialCards[i].classList.add("selected");
+        button.classList.add("selected");
+    });
+});
+
+// SELECT 1 TESTIMONIAL AND 1 BTN AT PAGE LOAD
+
+initializeCarousel();
